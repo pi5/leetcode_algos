bd20c0911ff878f3d209fec7ebedf9944de53307
Piyush Waradpande <pi5@umbc.edu>
2015-03-30 19:29:41 -0400

    171 -- Given excel sheet column, print column number

---
+++ 171_excel_sheet_col_number.cpp
@@ -0,0 +1,48 @@
+/* Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+
+*/
+
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int titleToNumber(string s) {
+    
+    if (s.length() == 0) {
+        return 0;    
+    }   
+
+    int len = s.length();
+    int power = 0;
+    int num = 0;
+    const int base = 26;
+
+    for (int i = 0; i < len; i++) {
+        int digit = s[i] - 'A' + 1;
+        num = num*26 + digit;
+    }
+    return num;
+}
+
+
+int main() {
+    
+    string s;
+    while(1) {
+        cin >> s;
+        cout << titleToNumber(s) << endl;
+    }
+}
