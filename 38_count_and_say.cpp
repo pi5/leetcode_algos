c20467e6f798a0d3996c0d525aa1c99535d9a52c
Piyush Waradpande <pi5@umbc.edu>
2015-04-11 00:06:55 -0400

    38 -- count and say

---
+++ 38_count_and_say.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string countAndSay(int n) {
+    if(n == 0) return "";
+
+    string num = "1" ;
+
+    for (int i = 1; i < n; i++) {
+        int curr = 0;
+        string next = "";
+        while(curr < num.length()) {
+            char c = num[curr];
+            int count = 0;
+            while(num[curr+count] == c && curr+count < num.length())
+                 count++;
+            next += (to_string(count) + c);
+            curr = curr+count;
+        }
+        num = next;
+        cout << num << endl;
+    }
+    return num;
+}
+
+int main() {
+
+    int n; 
+    cin >> n;
+    cout << countAndSay(n);
+
+}
