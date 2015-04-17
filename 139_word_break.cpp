1b6190865dfff8c5e24acd1b84f0f39a3e0ecca1
Piyush Waradpande <pi5@umbc.edu>
2015-04-16 23:38:09 -0400

    139 -- Adding another variant of DP solution. This imitates the recursive solution

--- 139_word_break.cpp
+++ 139_word_break.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 
 /* Recursive solution */
-bool wordBreak_util(const string &s, const unordered_set<string>& wordDict, int start, int end) {
+bool wordBreak_recursive_util(const string &s, const unordered_set<string>& wordDict, int start, int end) {
     if(wordDict.find(s.substr(start)) != wordDict.end()) {
         return true;    
     }
@@ -25,7 +25,7 @@ bool wordBreak_util(const string &s, const unordered_set<string>& wordDict, int 
     for (int i = start; i < end; i++) {
         string word = s.substr(start, i-start+1);
         if(wordDict.find(word) != wordDict.end()) {
-            found_in_rest = wordBreak_util(s, wordDict, i+1, end);            
+            found_in_rest = wordBreak_recursive_util(s, wordDict, i+1, end);            
         }
         if(found_in_rest) return true;
     }
@@ -33,14 +33,14 @@ bool wordBreak_util(const string &s, const unordered_set<string>& wordDict, int 
     return false;
 }
 
+/* Recursive Solution */
 bool wordBreak_recursive(string s, unordered_set<string>& wordDict) {
-    return wordBreak_util (s, wordDict, 0, s.length());
+    return wordBreak_recursive_util (s, wordDict, 0, s.length());
 }
 
 
-
-/* DP solution */
-bool wordBreak(string s, unordered_set<string>& wordDict) {
+/* DP solution 1*/
+bool wordBreak_dp1(string s, unordered_set<string>& wordDict) {
     
     bool memo[s.length()];
     memset(memo, false, sizeof(memo));
@@ -69,13 +69,49 @@ bool wordBreak(string s, unordered_set<string>& wordDict) {
 }
 
 
+/* DP solution 2. This solution imitates recursive solution */
+bool wordBreak_util(const string &s, const unordered_set<string>& wordDict, int start, int end, int memo[]) {
+    
+    if(memo[start] != -1) {
+        return memo[start];    
+    }
+
+    if(wordDict.find(s.substr(start)) != wordDict.end()) {
+        memo[start] = true;
+        return memo[start];    
+    }
+    
+    bool found_in_rest = false;
+    for (int i = start; i < end; i++) {
+        string word = s.substr(start, i-start+1);
+        if(wordDict.find(word) != wordDict.end()) {
+            found_in_rest = wordBreak_util(s, wordDict, i+1, end, memo);            
+        }
+        if(found_in_rest) {
+            memo[start] = true;
+            return memo[start];
+        }
+    }
+
+    memo[start] = false;
+    return memo[start];
+}
+
+/* DP solution 2 */
+bool wordBreak(string s, unordered_set<string>& wordDict) {
+    int memo[s.length()];
+    memset(memo, -1, sizeof(memo));
+    return wordBreak_util (s, wordDict, 0, s.length(), memo);
+}
+
+
 int main() {
     string s = "leetcode";
     unordered_set<string> dict;
-    dict.insert("leet");
+    dict.insert("leets");
     dict.insert("code");
     dict.insert("leetc");
-    dict.insert("odes");
+    dict.insert("codes");
 
     cout << wordBreak(s, dict) << endl;
 } 
